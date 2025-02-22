edbb40963b527e38cf9450d5d9b8ddd768d7faef
mpar12 <65396644+mpar12@users.noreply.github.com>
2025-02-22 14:45:15 -0500

    First Real Commit - I'm back? I'm back.

---
+++ GetMovingBuddy/GetMovingBuddy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GetMovingBuddy
+//
+//  Created by Mihir Parikh on 2/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GetMovingBuddy/GetMovingBuddy/GetMovingBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetMovingBuddyApp.swift
+//  GetMovingBuddy
+//
+//  Created by Mihir Parikh on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct GetMovingBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
